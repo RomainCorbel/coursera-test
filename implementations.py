3755d50340c04a18d3ce4495d5b25e9cbf2952d9
Romain <romain.corbel@hotmail.com>
2024-10-30 10:24:39 +0100

    17

--- implementations.py
+++ implementations.py
@@ -349,7 +349,7 @@ def learning_by_newton_method(y, tx, w, gamma):
     w = w-gamma*np.dot(np.linalg.inv(hessian),gradient)
     return w,loss
 '''
-def penalized_logistic_regression(y, tx, w, lambda_,max_iters,gamma):
+def penalized_logistic_regression(y,tx,lambda_,initial_w,max_iters,gamma):
     """return the loss and gradient.
 
     Args:
@@ -376,13 +376,13 @@ def penalized_logistic_regression(y, tx, w, lambda_,max_iters,gamma):
            [ 0.2467104 ],
            [ 0.57712843]])
     """
-    loss, gradient, _ = logistic_regression(y,tx,w,max_iters,gamma)
+    loss, gradient, _ = logistic_regression(y,tx,initial_w,max_iters,gamma)
     penalized_loss = loss + lambda_*np.linalg.norm(w)**2 
     penalized_gradient = gradient + 2*lambda_*w
 
     return loss, penalized_gradient
 
-def learning_by_penalized_gradient(y, tx, w,max_iters, gamma, lambda_):
+def learning_by_penalized_gradient(y,tx,lambda_,initial_w,max_iters,gamma):
     """
     Do one step of gradient descent, using the penalized logistic regression.
     Return the loss and updated w.
@@ -413,7 +413,7 @@ def learning_by_penalized_gradient(y, tx, w,max_iters, gamma, lambda_):
            [0.17532896],
            [0.24228716]])
     """
-    loss, gradient = penalized_logistic_regression(y, tx, w, lambda_,max_iters,gamma)
+    loss, gradient = penalized_logistic_regression(y,tx,lambda_,initial_w,max_iters,gamma)
     w = w-gamma*gradient
     return w,loss
 
@@ -451,7 +451,7 @@ def reg_logistic_regression(y,tx,lambda_,initial_w,max_iters,gamma):
     # start the logistic regression
     for iter in range(max_iters):
         # get loss and update w.
-        w,loss = learning_by_penalized_gradient(y, tx, w, gamma, lambda_)
+        w,loss = learning_by_penalized_gradient(y,tx,lambda_,initial_w,max_iters,gamma)
         # log info
         if iter % 100 == 0:
             print("Current iteration={i}, loss={l}".format(i=iter, l=loss))
